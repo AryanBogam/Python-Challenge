4e2293071341e2d54245859799ff590c79086ef4
AryanBogam <archanabogam@gmail.com>
2025-07-01 21:53:02 +0530

    Add problem_09

---
+++ day_01/problem_09/question.md
@@ -0,0 +1,3 @@
+"Dear Harry, This python course is nice. Thanks!"
+
+write this send in a form a letter
---
+++ day_01/problem_09/solution.py
@@ -0,0 +1,5 @@
+# making changes and add new sentence is \n in the given sentence.
+letter = "Dear Harry,\n\tThis python course is nice.\nThanks!"
+
+# Printing the final answer.
+print(letter)
