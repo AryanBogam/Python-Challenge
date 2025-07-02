271a9ff39dfc09bf0b45e7388420b465dad1cd0d
AryanBogam <archanabogam@gmail.com>
2025-07-02 19:25:06 +0530

    problem_01

---
+++ day_02/problem_01/question.md
@@ -0,0 +1,7 @@
+Weekend Plan Bot
+
+🧠 Problem:
+You have a list: ["gym", "study", "Netflix", "family", "read"]
+Ask the user for a number (0–4).
+Print what activity they’ll do that day.
+
---
+++ day_02/problem_01/solution.py
@@ -0,0 +1,8 @@
+# This is the given list in the question.
+list = ["gym", "study", "Netflix", "family", "read"]
+
+# Taking number input from the user between (0 - 4).
+user_input = int(input("Enter number between (0 - 4): "))
+
+# Printing what he will do.
+print(list[user_input])
