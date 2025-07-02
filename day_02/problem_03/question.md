c4f3616b3d6ba6455cd8858f5ae73973a80e1b06
AryanBogam <archanabogam@gmail.com>
2025-07-02 19:33:29 +0530

    Add problem_3

---
+++ day_02/problem_03/question.md
@@ -0,0 +1,8 @@
+Startup Stack Updater
+
+Problem:
+You have a list: ["Python", "JavaScript", "C++"]
+Add "Rust" to the list
+Replace "C++" with "Go"
+
+Print final list
---
+++ day_02/problem_03/solution.py
@@ -0,0 +1,11 @@
+# This is given list in question.
+list = ["Python", "JavaScript", "C++"]
+
+# .append is add "Rust" in list.
+list.append("Rust")
+
+# replacing "javaScript" in list to "Go".
+list[2] = "Go"
+
+# Printing final answer.
+print(list)
