ebc357b8d89c6ab63d22dd42dc15c7aba6158e61
AryanBogam <archanabogam@gmail.com>
2025-07-03 22:35:14 +0530

    Add problem_07

---
+++ day_03/problem_07/question.md
@@ -0,0 +1,3 @@
+Basic Tuple Creation with Different Data Types
+
+Question: Create a tuple with 4 elements: your age (integer), your height in feet (float), your name (string), and whether you like Python (boolean). Print the tuple and the type of each element.
---
+++ day_03/problem_07/solution.py
@@ -0,0 +1,11 @@
+# It is a tuple with different datatypes
+my_info = (20, 5.8, "John", True)
+
+# Printing the tuple
+print("My info tuple:", my_info)
+
+# Printing type of each element
+print("Age type:", type(my_info[0]))
+print("Height type:", type(my_info[1]))
+print("Name type:", type(my_info[2]))
+print("Likes Python type:", type(my_info[3]))
