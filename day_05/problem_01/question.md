30fae7c0e028f02194d13866bb850d31178bd314
AryanBogam <archanabogam@gmail.com>
2025-07-05 18:26:39 +0530

    Add problem_01

---
+++ day_05/problem_01/question.md
@@ -0,0 +1,3 @@
+Create a Dictionary
+
+Q: Create a dictionary with keys "name", "age", and "city" and assign your details to them.
---
+++ day_05/problem_01/solution.py
@@ -0,0 +1,8 @@
+# Creating a dictionary with personal details
+my_info = {
+    "name": "Aryan",
+    "age": 18,
+    "city": "Solapur"
+}
+
+print(my_info)
