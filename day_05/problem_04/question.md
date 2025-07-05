0e207d59674827cc2ce0bfad7a27e3090ded3a3e
AryanBogam <archanabogam@gmail.com>
2025-07-05 18:32:45 +0530

    Add problem_04

---
+++ day_05/problem_04/question.md
@@ -0,0 +1,3 @@
+Add a New Key
+Q: Add a new key "email" with any value to the same dictionary.
+
---
+++ day_05/problem_04/solution.py
@@ -0,0 +1,6 @@
+person = {'name': 'Aryan', 'age': 19, 'city': 'Solapur'}
+
+# Added new key "email"
+person['email'] = 'aryan@example.com'
+
+print(person)
