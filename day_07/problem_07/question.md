bd16a172650808eda40939ac84f8f3e46ed840c4
AryanBogam <archanabogam@gmail.com>
2025-07-07 16:19:31 +0530

    Add problem_07

---
+++ day_07/problem_07/question.md
@@ -0,0 +1,5 @@
+Q7. Secret Code Game
+
+Task:
+Let user guess a number until they get it right (e.g., 42)
+Tell them if it's too high or too low each time.
---
+++ day_07/problem_07/solution.py
@@ -0,0 +1,11 @@
+secret = 42
+guess = -1
+
+while guess != secret:
+    guess = int(input("Guess the number: "))
+    if guess < secret:
+        print("Too low!")
+    elif guess > secret:
+        print("Too high!")
+
+print("Correct! You cracked the code.")
