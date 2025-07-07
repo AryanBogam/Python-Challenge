47b606a84a711f13d32043151a9a40ef2deb7f49
AryanBogam <archanabogam@gmail.com>
2025-07-07 15:58:46 +0530

    Add problem_03

---
+++ day_07/problem_03/question.md
@@ -0,0 +1,4 @@
+Q3. Odd Number Printer
+
+Task:
+Print the first 10 odd numbers using a while loop.
---
+++ day_07/problem_03/solution.py
@@ -0,0 +1,8 @@
+count = 0
+num = 1
+
+while count < 10:
+    if num % 2 != 0:
+        print(num)
+        count += 1
+    num += 1
