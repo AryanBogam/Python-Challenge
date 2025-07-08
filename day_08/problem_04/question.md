c1833c151599694d9371bd75dae9443dbca136ab
AryanBogam <archanabogam@gmail.com>
2025-07-08 11:21:17 +0530

    Add problem_04

---
+++ day_08/problem_04/question.md
@@ -0,0 +1,7 @@
+Q6. Star Pattern Pyramid
+
+Input: 4 → Output:
+*
+**
+***
+****
---
+++ day_08/problem_04/solution.py
@@ -0,0 +1,5 @@
+# Taking n as input from user.
+n = int(input("Enter number: "))
+
+for i in range(1,n+1):
+    print("*"*i)
