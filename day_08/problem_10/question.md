8c61d18128ef60de7d02517d057b511b27fb042a
AryanBogam <archanabogam@gmail.com>
2025-07-08 12:04:37 +0530

    Add problem_10

---
+++ day_08/problem_10/question.md
@@ -0,0 +1,4 @@
+Q10. Find Max in List without using max()
+
+Input: [9, 4, 18, 11, 7]
+Output: 18
---
+++ day_08/problem_10/solution.py
@@ -0,0 +1,11 @@
+# Given in question.
+nums = [9, 4, 18, 11, 7]
+
+max_num = nums[0]
+for num in nums:
+    if num > max_num:
+        max_num = num
+print("Maximum number is:", max_num)
+
+
+# The question was difficult for me so i took help of chatgpt, and understood the concept behind it.
