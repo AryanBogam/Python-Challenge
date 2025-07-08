78e133c17f560fbf6bed3e04869ea615763c0386
AryanBogam <archanabogam@gmail.com>
2025-07-08 11:16:22 +0530

    Add problem_03

---
+++ day_08/problem_03/question.md
@@ -0,0 +1,5 @@
+Q3. Multiplication Table Generator
+
+Input: 7 → Output:
+7 x 1 = 7
+7 x 2 = 14 ...
---
+++ day_08/problem_03/solution.py
@@ -0,0 +1,5 @@
+# Taking n as the input from user.
+n = int(input("Enter number: "))
+
+for i in range(1,11):
+    print(f"{n} X {i} = {i*n}")
