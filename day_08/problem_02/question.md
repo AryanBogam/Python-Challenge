3c376277304a06edf25ee2042c19603b3481386a
AryanBogam <archanabogam@gmail.com>
2025-07-08 11:11:48 +0530

    Add problem_02

---
+++ day_08/problem_02/question.md
@@ -0,0 +1,4 @@
+Q2. Sum of First N Numbers
+
+Input: 5 → Output: 15
+(1 + 2 + 3 + 4 + 5)
---
+++ day_08/problem_02/solution.py
@@ -0,0 +1,8 @@
+# Taking n as user input.
+n = int(input("Enter number: "))
+
+sum = 0
+for i in range(1,n+1):
+    sum += i
+
+print(sum,end="")
