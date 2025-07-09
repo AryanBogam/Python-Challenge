bf75fac33ba14e95f5f227b339210808e37d497f
AryanBogam <archanabogam@gmail.com>
2025-07-09 06:21:49 +0530

    Add problem_02

---
+++ day_09/problem_02/question.md
@@ -0,0 +1,2 @@
+Q2. Sum of Two Numbers
+Write a function that accepts two numbers and returns their sum.
---
+++ day_09/problem_02/solution.py
@@ -0,0 +1,10 @@
+# Function to calculate the sum of two numbers.
+def add(x, y):
+    return f"The sum of number is {x + y}."
+
+# Get two numbers from the user.
+x = int(input("Enter number: "))
+y = int(input("Enter number: "))
+
+# display the final result.
+print(add(x,y))
