7a7971e08d979c37b9d77f6c1568d2ed89b98a56
AryanBogam <archanabogam@gmail.com>
2025-07-10 18:47:05 +0530

    Add problem_04

---
+++ day_10/problem_04/question.md
@@ -0,0 +1,4 @@
+Q4. Dictionary Basics
+student = {"name": "Aryan", "age": 18, "grade": "A"}
+Task: Add a new key "city" with value "Pune"
+Then update the "age" to 19 and print all keys and values.
---
+++ day_10/problem_04/solution.py
@@ -0,0 +1,10 @@
+# Given dictionary
+student = {"name": "Aryan", "age": 18, "grade": "A"}
+
+# Add city and update age
+student["city"] = "Pune"
+student["age"] = 19
+
+# Print keys and values
+for key, value in student.items():
+    print(key, ":", value)
