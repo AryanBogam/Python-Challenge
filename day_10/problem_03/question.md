703ca9f49390c76e04731550e79185cd38dc9191
AryanBogam <archanabogam@gmail.com>
2025-07-10 18:44:05 +0530

    Add problem_03

---
+++ day_10/problem_03/question.md
@@ -0,0 +1,3 @@
+Q3. String Reverse + Replace + Slice
+text = "hello_python_world"
+Task: Reverse the string, replace '_' with '-', and extract only every 2nd character from index 0 to 10.
---
+++ day_10/problem_03/solution.py
@@ -0,0 +1,9 @@
+# Given text
+text = "hello_python_world"
+
+
+# Reverse and replace '_' with '-'
+result = text[::-1].replace("_", "-")
+
+# Print every 2nd char from first 11
+print(result[0:11:2])
