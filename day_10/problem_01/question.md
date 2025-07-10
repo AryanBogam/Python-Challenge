7a2bf0687e63b82f8bf0857630ca5525b4ff102f
AryanBogam <archanabogam@gmail.com>
2025-07-10 18:36:09 +0530

    Add problem_01

---
+++ day_10/problem_01/question.md
@@ -0,0 +1,3 @@
+Q1. List Manipulation
+my_list = [5, 3, 8, 6, 7]
+Task: Write code to sort the list in reverse order and then remove the middle element.
---
+++ day_10/problem_01/solution.py
@@ -0,0 +1,11 @@
+# Given list
+my_list = [5, 3, 8, 6, 7]
+
+# Reverse the list
+my_list.sort(reverse=True)
+
+# Remove the middle element
+del my_list[len(my_list) // 2] 
+
+# print the list
+print(my_list)
