b34fbcf29a20f95588bcc1db7a7b2643ab1229a4
AryanBogam <archanabogam@gmail.com>
2025-07-10 18:55:58 +0530

    Add problem_07

---
+++ day_10/problem_07/question.md
@@ -0,0 +1,4 @@
+Q7. Sets
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+Task: Print union, intersection, and symmetric difference of sets a and b.
---
+++ day_10/problem_07/solution.py
@@ -0,0 +1,8 @@
+# Given Two sets
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+# Print final answers
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Symmetric Difference:", a ^ b)
