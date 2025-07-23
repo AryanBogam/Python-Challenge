7627f24c290ecc43cbd5850602197820449ed47c
AryanBogam <archanabogam@gmail.com>
2025-07-23 18:24:55 +0530

    Add problem_01

---
+++ day_23/problem_01/question.md
@@ -0,0 +1,10 @@
+Student Score Tracker – Immutable Logs
+Task:
+Create a tuple with students' names and their marks:
+Example:
+students = (("Aryan", 89), ("Riya", 92), ("Om", 76))
+
+Print:
+The student with the highest score
+The average score
+Total number of students
---
+++ day_23/problem_01/solution.py
@@ -0,0 +1,16 @@
+students = (("Aryan", 89), ("Riya", 92), ("Om", 76))
+
+# Top scorer
+topper = max(students, key=lambda x: x[1])  # Use lambda to access marks
+
+# Average score
+total = sum([score for name, score in students])
+average = total / len(students)
+
+# Total students
+total_students = len(students)
+
+# Final Answer.
+print(" Topper:", topper[0], "with", topper[1], "marks")
+print(" Average Score:", average)
+print(" Total Students:", total_students)
