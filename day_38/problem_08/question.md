a5796c88e2143b896a796a7bc75a3ef315885bce
AryanBogam <archanabogam@gmail.com>
2025-08-07 23:06:28 +0530

    Add problem_08

---
+++ day_38/problem_08/question.md
@@ -0,0 +1,8 @@
+Story View Counter
+Count how many people viewed your story.
+
+Problem:
+Write count_views(views) where views is a list of usernames who saw your story.
+
+Input: ["a", "b", "c", "d"]
+Output: 4
---
+++ day_38/problem_08/solution.py
@@ -0,0 +1,10 @@
+def count_views(views):
+    return len(views)
+
+views = ["a", "b", "c", "d"]
+result = count_views(views)
+print(result)
+
+views2 = ["user1", "user2", "user3", "user4", "user5", "user6"]
+result2 = count_views(views2)
+print(result2)
