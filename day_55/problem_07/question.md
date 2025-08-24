e3c9168679c9eaee176d35bde0009b596f4984e8
AryanBogam <archanabogam@gmail.com>
2025-08-24 23:18:10 +0530

    Add problem_07

---
+++ day_55/problem_07/question.md
@@ -0,0 +1,13 @@
+Q7. Matrix Transpose
+
+Task: Write a function to transpose a matrix using nested loops.
+Input:
+[[1, 2, 3],
+ [4, 5, 6]]
+
+Output:
+[[1, 4],
+ [2, 5],
+ [3, 6]]
+
+Why: Matrix operations appear in ML, AI, data processing.
---
+++ day_55/problem_07/solution.py
@@ -0,0 +1,15 @@
+def transpose_matrix(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transposed = []
+    
+    for j in range(cols):
+        new_row = []
+        for i in range(rows):
+            new_row.append(matrix[i][j])
+        transposed.append(new_row)
+    return transposed
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+result = transpose_matrix(matrix)
+print(result)
