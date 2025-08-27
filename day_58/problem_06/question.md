f3359313d9cf29c539d41e92b870008cab032cc4
AryanBogam <archanabogam@gmail.com>
2025-08-27 09:00:05 +0530

    add problem_06

---
+++ day_58/problem_06/question.md
@@ -0,0 +1,12 @@
+Q6. Hollow Triangle Pattern
+
+Task: Print a right triangle with * borders but spaces inside.
+Input: Rows = 4
+Output:
+
+*
+**
+* *
+****
+
+Why: Introduces conditions inside triangles for edges vs inner part.
---
+++ day_58/problem_06/solution.py
@@ -0,0 +1,10 @@
+rows = 4
+
+for i in range(1, rows + 1):
+    line = ""
+    for j in range(1, i + 1):
+        if i == 1 or i == rows or j == 1 or j == i:
+            line = line + "*"
+        else:
+            line = line + " "
+    print(line)
