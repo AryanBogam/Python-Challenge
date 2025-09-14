fa8cc6cf7106f64bed6ae7445aea50e3e94e492d
AryanBogam <archanabogam@gmail.com>
2025-09-14 15:28:24 +0530

    Add Edits

--- day_76/hotel_menu.py
+++ day_76/hotel_menu.py
@@ -15,6 +15,7 @@ print("Pizza: Rs40\nPasta: Rs60\nBurger: Rs60\nSalad: Rs70\nCoffe: Rs80")
 
 order_total = 0
 
+# Asking the first order.
 item_1 = input("Enter the name of item you want to order = ")
 if item_1 in menu:
     order_total += menu[item_1]
@@ -22,6 +23,7 @@ if item_1 in menu:
 else:
     print(f"Ordered item {item_1} is not avaiable yet!")
 
+# Asking the second order.
 another_order = input("Do you want to add another item? (Yes/No)")
 if another_order == "Yes" or "yes":
     item_2 = input("Enter the name of item you want to order = ")
