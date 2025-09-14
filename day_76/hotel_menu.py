de3b54332be370dff4deeeeb77651b826db47efb
AryanBogam <archanabogam@gmail.com>
2025-09-14 15:22:47 +0530

    Add second order in total orders amount

--- day_76/hotel_menu.py
+++ day_76/hotel_menu.py
@@ -23,3 +23,7 @@ else:
     print(f"Ordered item {item_1} is not avaiable yet!")
 
 another_order = input("Do you want to add another item? (Yes/No)")
+if another_order == "Yes" or "yes":
+    item_2 = input("Enter the name of item you want to order = ")
+    if item_2 in menu:
+        order_total += menu[item_1]
