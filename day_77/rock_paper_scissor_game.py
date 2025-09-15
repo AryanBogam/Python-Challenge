3e27514c0fac109073c2d25e40cb3c6856b68c0b
AryanBogam <archanabogam@gmail.com>
2025-09-15 19:35:11 +0530

    Add game algo to see how wins

--- day_77/rock_paper_scissor_game.py
+++ day_77/rock_paper_scissor_game.py
@@ -5,3 +5,21 @@ user_choice = input("Enter your move = Rock, Paper, Scissor= ")
 comp_choice = random.choice(item_list)
 
 print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
+
+if user_choice == comp_choice:
+    print("Both chooses same = Match Tie")
+elif user_choice == "Rock" or "rock":
+    if comp_choice == "Paper":
+        print("Paper covers Rock = Computer Wins")
+    else:
+        print("Rock smashes Scissor = You Win")
+elif user_choice == "Paper" or "paper":
+    if comp_choice == "Scissor":
+        print("Scissor cuts paper = Computer Wins")
+    else:
+        print("Paper covers rock = You Win")
+elif user_choice == "Scissor" or "scissor":
+    if comp_choice == "Paper":
+        print("Scissor cuts paper = You Win")
+    else:
+        print("Rock smashes scissor = Computer Wins")
