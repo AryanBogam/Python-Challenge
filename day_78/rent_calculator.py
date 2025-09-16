931278c56b1e4aa7f34bcc58e6e8c7dd8e35ce62
AryanBogam <archanabogam@gmail.com>
2025-09-16 19:45:18 +0530

    calculate total amount for each persons

--- day_78/rent_calculator.py
+++ day_78/rent_calculator.py
@@ -2,6 +2,7 @@ rent = int(input("Enter your hostel/flat rent = "))
 food = int(input("Enter the amount of food ordered = "))
 electricity = int(input("Enter the total of electricity spend = "))
 charge_per_unit = int(input("Enter the charge per unit = "))
-charge_per_unit = int(input("Enter the charge per unit = "))
+persons = int(input("Enter the number of persons living in room/flat = "))
 
 total_bill = electricity * charge_per_unit
+output = (food + rent + total_bill) // persons
