163158e2420df62e7ab1e14481b8943c02d8c4ca
AryanBogam <archanabogam@gmail.com>
2025-09-16 19:41:59 +0530

    Ask the charge per unit

--- day_78/rent_calculator.py
+++ day_78/rent_calculator.py
@@ -1,3 +1,4 @@
 rent = int(input("Enter your hostel/flat rent = "))
 food = int(input("Enter the amount of food ordered = "))
 electricity = int(input("Enter the total of electricity spend = "))
+charge_per_unit = int(input("Enter the charge per unit = "))
