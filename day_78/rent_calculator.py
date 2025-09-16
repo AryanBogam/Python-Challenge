1aed2026dffc3aeb8b46102c74ff87c2ffc66374
AryanBogam <archanabogam@gmail.com>
2025-09-16 19:43:38 +0530

    Multiply electricity with charge per unit

--- day_78/rent_calculator.py
+++ day_78/rent_calculator.py
@@ -3,3 +3,5 @@ food = int(input("Enter the amount of food ordered = "))
 electricity = int(input("Enter the total of electricity spend = "))
 charge_per_unit = int(input("Enter the charge per unit = "))
 charge_per_unit = int(input("Enter the charge per unit = "))
+
+total_bill = electricity * charge_per_unit
