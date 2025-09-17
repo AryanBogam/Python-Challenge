af8a6a9ce677d8a9a897a9fc1c2b01396a918983
AryanBogam <archanabogam@gmail.com>
2025-09-17 22:12:37 +0530

    add function to add new students

--- day_79/students_grade_system.py
+++ day_79/students_grade_system.py
@@ -1,2 +1,12 @@
 # Initialising dictionary to store student names and grades
 student_grades = { }
+
+
+# Function to add a new student
+def add_student(name, grade):
+    # Store student name and grade in dictionary
+    student_grades[name] = grade
+    # [sagar] = 100
+    # Print confirmation message
+    print(f"Added {name} with a {grade}")
+    # Added sagar with a 100
