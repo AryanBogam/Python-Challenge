acc46f7e5ac57dc22ec6180574515226f15dfeff
AryanBogam <archanabogam@gmail.com>
2025-09-17 22:13:09 +0530

    add function to display all students

--- day_79/students_grade_system.py
+++ day_79/students_grade_system.py
@@ -10,3 +10,16 @@ def add_student(name, grade):
     # Print confirmation message
     print(f"Added {name} with a {grade}")
     # Added sagar with a 100
+
+
+# Function to view all students and their grades
+def display_all_students():
+    # Check if dictionary has any students
+    if student_grades:
+        # Loop through all students in dictionary
+        for name, grade in student_grades.items():
+            # Print each student's name and grade
+            print(f"{name} : {grade}")
+    else:
+        # Print message if no students found
+        print("No students found/Added")
