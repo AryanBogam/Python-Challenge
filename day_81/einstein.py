9c544f38f193d268db297f0559f4f04a6925d8d4
AryanBogam <archanabogam@gmail.com>
2025-09-19 21:32:27 +0530

    Add problem_04

---
+++ day_81/einstein.py
@@ -0,0 +1,4 @@
+user_input = int(input("M: "))  # It is takes the input of the value M.
+
+SPEED_OF_LIGHT = (user_input*300000000**2)  # It is the formulla of E = MC^2
+print(f"E: {SPEED_OF_LIGHT}")  # It prints the ans of E(energy).
