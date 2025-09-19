a60d279b390717b972b9305d6dbc747a68f5c8f2
AryanBogam <archanabogam@gmail.com>
2025-09-19 21:30:56 +0530

    Add problem_03

---
+++ day_81/faces.py
@@ -0,0 +1,4 @@
+user_input = input() # It is taking user input.
+
+user_input = user_input.replace(":)","🙂").replace(":(","🙁") # It is replacing the sing with the emoji.
+print(user_input) # It is print the final result.
